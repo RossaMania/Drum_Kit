aad50e0ef5c4a159a2a655a6cadd329313103317
Ross Clettenberg <raclettenberg@gmail.com>
2022-02-28 21:52:31 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (i = 0; i<numberOfDrumButtons; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    alert("I got clicked!");
+
+});
+
+}
