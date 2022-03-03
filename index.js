b1e246d18d6951bb4084273c079defc4d53b20f4
Ross Clettenberg <raclettenberg@gmail.com>
2022-03-02 19:43:27 -0600

    Added background images to buttons. Changed color of buttons when clicked. Added a sound.

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ for (i = 0; i<numberOfDrumButtons; i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-    alert("I got clicked!");
+this.style.color = "white";    
 
 });
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
