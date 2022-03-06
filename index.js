a1bd9832aad9d74789142a065dcabf4c183c8f9a
Ross Clettenberg <raclettenberg@gmail.com>
2022-03-05 20:44:59 -0600

    Added animation for when buttons are pressed.

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var buttonInnerHTML = this.innerHTML;
 
 makeSound(buttonInnerHTML);
 
+buttonAnimation(buttonInnerHTML);
+
 });
 
 }
@@ -20,6 +22,8 @@ makeSound(buttonInnerHTML);
 document.addEventListener("keydown", function(event) {
 
     makeSound(event.key);
+
+    buttonAnimation(event.key);
     
     });
 
@@ -68,7 +72,19 @@ function makeSound(key) {
     }
 }
 
+function buttonAnimation(currentKey) {
 
+var activeButton = document.querySelector("." + currentKey);
+
+activeButton.classList.add("pressed");
+
+setTimeout(function() {
+    
+    activeButton.classList.remove("pressed");
+    
+}, 100);
+
+}
 
 
 
